2f1b3ffeabb294087db62b39dc5525e9ea54adb0
vini <viniciusrebelo487@gmail.com>
2021-01-26 21:45:56 -0300

    Fix bug App

--- pages/room/src/view.js
+++ pages/room/src/view.js
@@ -1,18 +1,22 @@
-class view {
+class View {
   constructor() {}
 
   createVideoElement({ muted = true, src, srcObject }) {
     const video = document.createElement("video");
-    video.mute = muted;
+    video.muted = muted;
     video.src = src;
+    video.srcObject = srcObject;
+
     if (src) {
       video.controls = true;
       video.loop = true;
-      Util.sleep(200).then((_) => video.play);
+      Util.sleep(200).then((_) => video.play());
     }
+
     if (srcObject) {
-      video.addEventListener("loadeddata", (_) => video.play());
+      video.addEventListener("loadedmetadata", (_) => video.play());
     }
+
     return video;
   }
 
